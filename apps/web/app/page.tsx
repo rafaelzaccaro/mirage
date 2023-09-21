f4153ae966ba1ee35082207d62a1a94c13187e0e
Rafael Zaccaro <rafael.zaccaro2@gmail.com>
2023-09-21 16:26:51 -0300

    API fetch example

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,13 @@
 export default async function Home() {
-  const greeting = 'home';
-  return <div>{greeting}</div>;
+  const res = await fetch('http://localhost:7777/glimpses');
+  const data = await res.json()
+  console.log(data)
+  return <div>{data.map((d: any) => {
+    return (
+      <div key={d.id}>
+        <div>{ d.slug }</div>
+        <div>{ d.isPublic.toString() }</div>
+      </div>
+    )
+  })}</div>;
 }
